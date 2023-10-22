819f6458f32097ccdc20c65067693af8b1005327
Sudheer-webDeveloper <sudheertalaudi@gmail.com>
2023-10-22 14:42:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Index from "./FirstProject";
+
+function App() {
+
+
+  return (
+    <Index />
+  )
+
+}
+
+export default App;
---
+++ src/FirstProject/index.jsx
@@ -0,0 +1,20 @@
+
+import Review from './Review'
+import './second.css'
+
+
+function Index() {
+
+  return (
+    <div className='index'>
+      <h1>First Project</h1>
+      <div>
+        <h2>our reviews</h2>
+        <Review />
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default Index
